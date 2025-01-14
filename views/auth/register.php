757d7e50473faaca885c0bbc0688147fde15d888
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-14 19:42:01 +0100

    add registration form to auth view

---
+++ views/auth/register.php
@@ -0,0 +1,85 @@
+<!-- sign up form-->
+<div class="hidden opacity-0 transition-opacity duration-150 ease-linear">
+    <!-- heading   -->
+    <div class="text-center">
+        <h3
+            class="text-size-32 font-bold text-blackColor dark:text-blackColor-dark mb-2 leading-normal">
+            Sign Up
+        </h3>
+    </div>
+
+    <form class="pt-25px" data-aos="fade-up">
+        <div
+            class="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
+            <div>
+            <label
+                class="text-contentColor dark:text-contentColor-dark mb-10px block">First Name</label>
+            <input
+                type="text"
+                placeholder="First Name"
+                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded">
+            </div>
+            <div>
+            <label
+                class="text-contentColor dark:text-contentColor-dark mb-10px block">Last Name</label>
+            <input
+                type="text"
+                placeholder="Last Name"
+                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded">
+            </div>
+        </div>
+        <div
+            class="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
+            <div>
+            <label
+                class="text-contentColor dark:text-contentColor-dark mb-10px block">Username</label>
+            <input
+                type="text"
+                placeholder="Username"
+                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded">
+            </div>
+            <div>
+            <label
+                class="text-contentColor dark:text-contentColor-dark mb-10px block">Email</label>
+            <input
+                type="email"
+                placeholder="Your Email"
+                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded">
+            </div>
+        </div>
+        <div
+            class="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
+            <div>
+            <label
+                class="text-contentColor dark:text-contentColor-dark mb-10px block">Password</label>
+            <input
+                type="password"
+                placeholder="Password"
+                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded">
+            </div>
+            <div>
+                <label
+                    class="text-contentColor dark:text-contentColor-dark mb-10px block">Re-Enter Password</label>
+                <input
+                    type="password"
+                    placeholder="Re-Enter Password"
+                    class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded">
+            </div>
+        </div>
+        <div>
+            <label class="text-contentColor dark:text-contentColor-dark mb-10px block">Choose what you want to be</label>
+            <select name="" id="" class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded">
+                <option class="text-black" value="student">Student</option>
+                <option value="teacher">Teacher</option>
+            </select>
+        </div>
+        
+        <div class="mt-25px text-center">
+            <button
+            type="submit"
+            class="text-size-15 text-whiteColor bg-primaryColor px-25px py-10px w-full border border-primaryColor hover:text-primaryColor hover:bg-whiteColor inline-block rounded group dark:hover:text-whiteColor dark:hover:bg-whiteColor-dark">
+            Sign up
+            </button>
+        </div>
+    </form>
+</div>
