43d4174110467138483f0ad6d841fbf0edff33b4
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-14 23:18:37 +0100

    add User class for user management and validation

---
+++ classes/User.php
@@ -0,0 +1,33 @@
+<?php
+
+    class User {
+        protected $username;
+        protected $firstName;
+        protected $lastName;
+        protected $email;
+        protected $password;
+        protected $role;
+        protected $status;
+        protected $image;
+        protected $conn;
+
+        public function __construct($conn, $username = "", $firstName = "", $lastName = "", $email = "", $password = "", $role = "", $status = "")
+        {
+            $this->username = $username;
+            $this->firstName = $firstName;
+            $this->lastName = $lastName;
+            $this->email = $email;
+            $this->password = $password;
+            $this->role = $role;
+            $this->status = $status;
+            $this->conn = $conn;
+        }
+
+        public function checkUsernameEmail() {
+            $sql = "SELECT username, email FROM users";
+            $result = $this->conn->query($sql);
+            return $result->fetch();
+        }
+    }
+
+?>
