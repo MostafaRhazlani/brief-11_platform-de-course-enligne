66d591dd2d188dc4003d987353298da7af79cd75
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-16 17:12:19 +0100

    fix update category logic for redirect to page categories

--- views/user/admin/crudCategory/updateCategory.php
+++ views/user/admin/crudCategory/updateCategory.php
@@ -8,15 +8,15 @@
     
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $idCategory = $_POST['idCategory'];
-        
+
         $category = new Category($conn, $idCategory);
 
-        if($category->getId() === $_POST['idCategory']) {
+        if($category->getId() === $idCategory) {
             $category->setNameCategory($_POST['nameCategory']);
-
-            if($category->updateCategoty()) {
-                header('location: /views/user/admin/categories.php');
-            }
+            $category->updateCategoty();
+            header('location: /views/user/admin/categories.php');
+        } else {
+            header('location: /views/user/admin/categories.php');
         }
     }
 
