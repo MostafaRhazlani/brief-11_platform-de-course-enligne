b836ef2243d1aba55e67a933080dea02592b99d9
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-19 19:08:45 +0100

    fix: correct path for Role class inclusion in admin users view

--- views/user/admin/users.php
+++ views/user/admin/users.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . '/../../classes/Role.php';   
+    require_once __DIR__ . '/../../../classes/Role.php';   
     require_once __DIR__ . '/../../../classes/Database.php';
     require_once __DIR__ . '/../../../classes/User.php';
 
